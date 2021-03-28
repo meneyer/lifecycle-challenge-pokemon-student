71f8bcedf2cc1f52f253206802765f41c1a464a0
Megan Neyer <Megan.Neyer@gmail.com>
2021-03-28 18:05:23 -0400

    pokemon finished

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import './PokeFetch.css';
 
-
 class PokeFetch extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      currentCount: 10,
+      intervalId: '',
+      pokemonAlive: false,
     }
   }
 
@@ -24,19 +26,42 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          pokemonAlive: false,
+          currentCount: 10,
         })
       })
       .catch((err) => console.log(err))
   }
 
+  timer() {
+    let intervalId = setInterval(() => this.tick(), 1000);
+    this.setState({intervalId: intervalId})
+  }
+  
+  tick(){
+    if (this.state.currentCount > 0) {
+      this.setState({ currentCount: this.state.currentCount - 1 })
+  } else {
+    clearInterval(this.intervalId)
+    this.setState({pokemonAlive: true})
+  }}
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.pokeName !== this.state.pokeName){
+    this.timer()
+    }
+  }
+
+
   render() {
     return (
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <button className={'start'} 
+            onClick={() => this.fetchPokemon()}>Start!</button>
+        <h1 className={'timer'} >{this.state.currentCount}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={'pokeImg'} style={this.state.pokemonAlive ? { filter: "brightness(100%)" }: { filter: "brightness(0%)" }} src={this.state.pokeSprite} />
+          <h1 className={'pokeName'} style={this.state.pokemonAlive ? { display: "" }: { display: "none" }}>{this.state.pokeName} </h1>
         </div>
       </div>
     )
